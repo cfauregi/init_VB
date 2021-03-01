c3e36904fb07370abcfe3a752ec4aa86ec3cf4b4
cfauregi <78905373+cfauregi@users.noreply.github.com>
2021-03-01 14:01:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,15 @@
 #include "fonction.h" 
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/adc.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/usart.h>
 
 int main()
 {
 int msk;
-int b=0,c=0;
-volatile int i;
-char le_test[80];
-le_test[80]='\n';
-//char data_freq_min2[80];
-
+int b=0;
 
 msk=(1<<11)|(1<<12)|(1<<13);
 clock_setup();
 init_gpio();
 adc_setup();
 
- for (i=0; i<150000;i++){}
 while(1)
   {
 	  if (b<1500000)
